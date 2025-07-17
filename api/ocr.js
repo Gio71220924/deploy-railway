9756255e2a6dc929cc6a9be42bc16142d0451d23
Giovanka Steviano Harry Premono <giovanka.steviano@ti.ukdw.ac.id>
2025-07-17 21:44:07 +0700

    Update

--- api/ocr.js
+++ api/ocr.js
@@ -1,27 +1,28 @@
-const multer = require('multer');
-const Tesseract = require('tesseract.js');
+const Tesseract = require("tesseract.js");
+const Busboy = require("busboy");
 
-const upload = multer({ storage: multer.memoryStorage() });
+module.exports = (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Only POST allowed" });
+  }
 
-module.exports = async (req, res) => {
-  if (req.method !== 'POST') return res.status(405).send('Use POST');
-
-  const busboy = require('busboy');
-  const bb = busboy({ headers: req.headers });
+  const bb = Busboy({ headers: req.headers });
   let buffer = Buffer.alloc(0);
 
-  bb.on('file', (_name, file) => {
-    file.on('data', data => (buffer = Buffer.concat([buffer, data])));
+  bb.on("file", (_name, file) => {
+    file.on("data", (data) => {
+      buffer = Buffer.concat([buffer, data]);
+    });
   });
 
-  bb.on('finish', async () => {
+  bb.on("finish", async () => {
     try {
       const {
         data: { text },
-      } = await Tesseract.recognize(buffer, 'eng+ind');
-      res.json({ text });
-    } catch (e) {
-      res.status(500).json({ error: e.message });
+      } = await Tesseract.recognize(buffer, "eng+ind");
+      res.status(200).json({ text });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
     }
   });
 
